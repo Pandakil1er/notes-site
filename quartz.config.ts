c05374ee3e47b0c98fdf68c69b8234d328211148
Pandakil1er <anshajsingh61@gmail.com>
2026-04-06 06:14:15 +0530

    added site

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ansuzks.me/notes-site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
